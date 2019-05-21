4cdfb6e318430acfd31ce4da6a96b96be83eb4d1
harshvijayvargiya <ror.training0@gmail.com>
2019-05-21 15:18:21 +0530

    add model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_083650) do
+ActiveRecord::Schema.define(version: 2019_05_21_094734) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_05_21_083650) do
   end
 
   create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "welcomes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
